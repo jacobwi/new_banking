c355dee598d26b92251a3581d974d25a230e9b2f
amjedcha <amjedcha@users.noreply.github.com>
2017-10-18 17:14:15 -0400

    Functional update

--- server.lua
+++ server.lua
@@ -2,7 +2,73 @@
 --==                                VARIABLES - DO NOT EDIT                                     ==
 --================================================================================================
 ESX = nil
-local balances = {}
-local user = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('bank:deposit')
+AddEventHandler('bank:deposit', function(amount)
+	local _source = source
+	
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	if amount == nil or amount <= 0 or amount > xPlayer.getMoney() then
+		TriggerClientEvent('chatMessage', _source, "Invalid Amount")
+	else
+		xPlayer.removeMoney(amount)
+		xPlayer.addAccountMoney('bank', tonumber(amount))
+		TriggerClientEvent('chatMessage', _source, xPlayer.getBank())
+	end
+end)
+
+
+RegisterServerEvent('bank:withdraw')
+AddEventHandler('bank:withdraw', function(amount)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local base = 0
+	amount = tonumber(amount)
+	base = xPlayer.getAccount('bank').money
+	TriggerClientEvent('chatMessage', _source, base)
+	if amount == nil or amount <= 0 or amount > base then
+		TriggerClientEvent('chatMessage', _source, "Invalid Amount")
+	else
+		xPlayer.removeAccountMoney('bank', amount)
+		xPlayer.addMoney(amount)
+	end
+end)
+
+RegisterServerEvent('bank:balance')
+AddEventHandler('bank:balance', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	balance = xPlayer.getAccount('bank').money
+	TriggerClientEvent('currentbalance1', _source, balance)
+	
+end)
+
+
+RegisterServerEvent('bank:transfer')
+AddEventHandler('bank:transfer', function(to, amountt)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local zPlayer = ESX.GetPlayerFromId(to)
+	local balance = 0
+	balance = xPlayer.getAccount('bank').money
+	zbalance = zPlayer.getAccount('bank').money
+	
+	if tonumber(_source) == tonumber(to) then
+		TriggerClientEvent('chatMessage', _source, "You cannot transfer to your self")
+	else
+		if balance <= 0 or balance < tonumber(amountt) then
+			TriggerClientEvent('chatMessage', _source, "You don't have enough money in the bank.")
+		else
+			xPlayer.removeAccountMoney('bank', amountt)
+			zPlayer.addAccountMoney('bank', amountt)
+		end
+		
+	end
+end)
+
+
+
+
+
